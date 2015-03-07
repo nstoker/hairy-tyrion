a2d142bfe95e19381eafadebdc7784149018abbc
Neil Stoker <neil.stoker@gmail.com>
2015-03-07 13:37:10 +0000

    Initial add of database class

---
+++ RockTrack/Source/database.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    database.h
+    Created: 7 Mar 2015 1:35:40pm
+    Author:  user
+
+  ==============================================================================
+*/
+
+#ifndef DATABASE_H_INCLUDED
+#define DATABASE_H_INCLUDED
+
+
+
+
+
+#endif  // DATABASE_H_INCLUDED
