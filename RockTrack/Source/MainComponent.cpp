e308e2f3957ffd67d418a3617100d44467bc2c93
Neil Stoker <neil.stoker@gmail.com>
2015-03-07 14:08:10 +0000

    Change welcome text

--- RockTrack/Source/MainComponent.cpp
+++ RockTrack/Source/MainComponent.cpp
@@ -25,7 +25,7 @@ void MainContentComponent::paint (Graphics& g)
 
     g.setFont (Font (16.0f));
     g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    g.drawText ("Hello world of Rocksmith!", getLocalBounds(), Justification::centred, true);
 }
 
 void MainContentComponent::resized()
