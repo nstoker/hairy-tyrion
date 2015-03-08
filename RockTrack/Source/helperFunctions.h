ef6e3358b86bdc5d9bf7b808c1fb6cd6df82563a
Neil Stoker <neil.stoker@gmail.com>
2015-03-08 22:05:55 +0000

    Add label helper function

---
+++ RockTrack/Source/helperFunctions.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    helperFunctions.h
+    Created: 8 Mar 2015 9:29:56pm
+    Author:  user
+
+  ==============================================================================
+*/
+
+#ifndef HELPERFUNCTIONS_H_INCLUDED
+#define HELPERFUNCTIONS_H_INCLUDED
+
+#include "JuceHeader.h"
+
+void setLabel(Label* theLabel, Font theFont, Justification theJustification, Colour theTextColour);
+
+#endif  // HELPERFUNCTIONS_H_INCLUDED
