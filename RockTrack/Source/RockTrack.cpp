6a4e8bf358be644af13785cf4007832c57ccf701
Neil Stoker <neil.stoker@gmail.com>
2015-03-14 15:16:53 +0000

    Add Rocktrack class

---
+++ RockTrack/Source/RockTrack.cpp
@@ -0,0 +1,14 @@
+/*
+  ==============================================================================
+
+    RockTrack.cpp
+    Created: 14 Mar 2015 3:05:31pm
+    Author:  user
+
+  ==============================================================================
+*/
+
+#include "RockTrack.h"
+
+ScopedPointer<CDatabase> rockTrackDB;
+ScopedPointer<FileLogger> theLog;
---
+++ RockTrack/Source/RockTrack.h
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    RockTrack.h
+    Created: 14 Mar 2015 2:50:02pm
+    Author:  user
+
+  ==============================================================================
+*/
+
+#ifndef ROCKTRACK_H_INCLUDED
+#define ROCKTRACK_H_INCLUDED
+
+#include "Database.h"
+#include "helperFunctions.h"
+
+extern ScopedPointer<CDatabase> rockTrackDB;
+extern ScopedPointer<FileLogger> theLog;
+
+
+
+
+#endif  // ROCKTRACK_H_INCLUDED
